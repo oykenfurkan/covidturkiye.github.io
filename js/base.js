0a78e835b5bdff323b28780c2a5f2b9b9ebdc566
oykenfurkan <oykenfurkan@gmail.com>
2020-04-05 01:49:12 +0300

    Stadia Map silindi, tooltip eklendi

--- js/base.js
+++ js/base.js
@@ -13,9 +13,10 @@
     map.options.minZoom = 7;
     map.options.maxZoom = 14;
 	
-    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-    	maxZoom: 20,
-    	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png', {
+    	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    	subdomains: 'abcd',
+    	maxZoom: 19
     }).addTo(map);
     
 
@@ -25,6 +26,9 @@
     }).addTo(map);
 
     function sehirFeature(feature, layer) {        
+        layer.bindTooltip(feature.properties.name,
+            {permanent: true, direction:"center"}
+        ).openTooltip();
 	    layer.bindPopup("<a style='font-size: 15px;'><b>" + feature.properties.name + "</b></a>"+
         "<br><table class='infoTable'>" +
         "<tbody><tr><td>Vaka Sayısı</td><td>" + feature.properties.vaka_sayisi + "</td></tr>"+
